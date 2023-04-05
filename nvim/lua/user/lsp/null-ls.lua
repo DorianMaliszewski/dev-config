b88434cb6363f0ae09083c6775b286451a66c2cf
Dorian Maliszewski <dmaliszewski@scaleway.com>
2023-04-05 12:25:18 +0200

    chore: remove useless var

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -8,8 +8,6 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-local codeActions = null_ls.builtins.code_actions
-
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
