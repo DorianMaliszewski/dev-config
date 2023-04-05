856a3fd93143cb63d83ae90e441555238a82d4ff
Dorian Maliszewski <dmaliszewski@scaleway.com>
2023-04-05 13:02:36 +0200

    fix: noice with nvim-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require('user.lazy')
+require("user.lazy")
 require("user.plugins")
 require("user.options")
 require("user.keymaps")
--- nvim/lua/user/noice.lua
+++ nvim/lua/user/noice.lua
@@ -4,7 +4,7 @@ require("noice").setup({
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true,
+			--[[ ["cmp.entry.get_documentation"] = true, ]]
 		},
 	},
 	-- you can enable a preset for easier configuration
@@ -13,6 +13,6 @@ require("noice").setup({
 		command_palette = true, -- position the cmdline and popupmenu together
 		long_message_to_split = false, -- long messages will be sent to a split
 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = true, -- add a border to hover docs and signature help
+		lsp_doc_border = false, -- add a border to hover docs and signature help
 	},
 })
