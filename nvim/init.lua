4465d7405387bf3b1fef095976e6002014ba0a8f
Dorian Maliszewski <maliszewskid3@gmail.com>
2025-03-30 16:35:53 +0200

    feat: improve

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 ---@diagnostic disable: different-requires
 require("user.options")
-require("user.lazy")
+require("user.lazy_install")
 require("user.plugins")
 require("user.keymaps")
 require("user.autocommands")
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -16,6 +16,16 @@ require("lazy").setup({
   require('user.snacks'),
   require('user.whichkey'),
   require('user.move'),
+  {
+    'MagicDuck/grug-far.nvim',
+    config = function()
+      require('grug-far').setup({
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      });
+    end
+  },
   -- Auto editing
   {
     "numToStr/Comment.nvim",
